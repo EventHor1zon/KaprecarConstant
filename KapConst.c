2c2db6a787bc06a287cd3976fd9488ecdf568554
EventHor1zon <3v3ntH0r1z0n@protonmail.com>
2023-01-18 22:05:51 +0000

    source template

---
+++ KapConst.c
@@ -0,0 +1,24 @@
+/**
+ * @file    KapConst.c 
+ * @author  RJAM
+ * @brief   Heard about a cool number called Kaprekar's Constant
+ *          6147. If you order the numbers, then subtract the reverse ordered 
+ *          number, you get back to 6147. Aparently doing this operation recursively 
+ *          on any 4-digit number results in 6147. Let's see...
+ * 
+ * 
+ **/
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+
+
+
+    return 0;
+}
